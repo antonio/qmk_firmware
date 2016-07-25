5398370365307d72bbe62b86257f59d38dfef248
Antonio Santos <antonio@santosvelasco.com>
2016-07-25 07:16:56 +0200

    More keymap changes

--- keyboards/ergodox_ez/keymaps/antonio/keymap.c
+++ keyboards/ergodox_ez/keymaps/antonio/keymap.c
@@ -35,19 +35,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [BASE] = KEYMAP(  // layer 0 : default
         // left hand
         KC_GRAVE,       KC_1,         KC_2,   KC_3,   KC_4,   KC_5,   KC_6,
-        KC_TAB,         KC_Q,         KC_W,   KC_E,   KC_R,   KC_T,   KC_RBRC,
+        KC_TAB,         KC_Q,         KC_W,   KC_E,   KC_R,   KC_T,   KC_LBRC,
         CTL_T(KC_ESC),  KC_A,         KC_S,   KC_D,   KC_F,   KC_G,
         KC_LSPO,        KC_Z,         KC_X,   KC_C,   KC_V,   KC_B,   KC_NO,
-        KC_NO,          KC_NO,        KC_NO,  KC_NO,  KC_LALT,
+        KC_NO,          KC_NO,        KC_NO,  LCTL(S(KC_TAB)),  KC_LALT,
         KC_NO,  KC_NO,
         KC_NO,
         GUI_T(KC_SPACE),TG(MOVEMENT),KC_NO,
         // right hand
         KC_7,     KC_8,   KC_9,   KC_0,   KC_9,   KC_0,  KC_MINS, KC_EQUAL, KC_BSPC,
-        KC_LBRC,  KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,  KC_BSLS,
+        KC_RBRC,  KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,  KC_BSLS,
         KC_H,     KC_J,   KC_K,   KC_L,   KC_SCLN,CTL_T(KC_QUOT),
         KC_NO,    KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH, KC_RSPO,
-        KC_UP,    KC_DOWN,KC_LBRC,KC_RBRC,         KC_FN1,
+        KC_NO,    KC_NO,  KC_LBRC,KC_RBRC,         KC_FN1,
         KC_LALT,  CTL_T(KC_ESC),
         KC_PGUP,
         KC_PGDN,KC_TAB, KC_ENT
