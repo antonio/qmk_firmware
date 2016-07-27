5a1b38947d3f508d85dc6a5e1320a23cc0d36d18
Antonio Santos <antonio@santosvelasco.com>
2016-07-27 07:30:30 +0200

    Fix format

--- keyboards/ergodox_ez/keymaps/antonio/keymap.c
+++ keyboards/ergodox_ez/keymaps/antonio/keymap.c
@@ -43,27 +43,27 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // If it accepts an argument (i.e, is a function), it doesn't need KC_.
 // Otherwise, it needs KC_*
 [BASE] = KEYMAP(  // layer 0 : default
-        // left hand
-        KC_GRAVE,       KC_1,        KC_2,   KC_3,   KC_4,     KC_5,   KC_6,
-        KC_TAB,         KC_Q,        KC_W,   KC_E,   KC_R,     KC_T,   KC_LBRC,
-        CTL_T(KC_ESC),  KC_A,        KC_S,   KC_D,   KC_F,     KC_G,
-        KC_LSPO,        KC_Z,        KC_X,   KC_C,   KC_V,     KC_B,   KC_NO,
-        KC_NO,          KC_NO,       KC_NO,  LCTL(S(KC_TAB)),  KC_LALT,
+// left hand
+KC_GRAVE,       KC_1,   KC_2,   KC_3,             KC_4,     KC_5,   KC_6,
+KC_TAB,         KC_Q,   KC_W,   KC_E,             KC_R,     KC_T,   KC_LBRC,
+CTL_T(KC_ESC),  KC_A,   KC_S,   KC_D,             KC_F,     KC_G,
+KC_LSPO,        KC_Z,   KC_X,   KC_C,             KC_V,     KC_B,   KC_NO,
+KC_NO,          KC_NO,  KC_NO,  LCTL(S(KC_TAB)),  KC_LALT,
 
-        KC_NO,          TG(GAMES),
-        KC_NO,
-        GUI_T(KC_SPACE),MO(MOVEMENT),KC_NO,
+                                                    KC_NO,  TG(GAMES),
+                                                                KC_NO,
+                                  GUI_T(KC_SPACE), MO(MOVEMENT), KC_NO,
 
-        // right hand
-        KC_7,        KC_8,           KC_9,   KC_0,   KC_MINS,  KC_EQUAL, KC_BSPC,
-        KC_RBRC,     KC_Y,           KC_U,   KC_I,   KC_O,     KC_P,            KC_BSLS,
-        KC_H,        KC_J,           KC_K,   KC_L,   KC_SCLN,  CTL_T(KC_QUOT),
-        KC_NO,       KC_N,           KC_M,   KC_COMM,KC_DOT,   KC_SLSH,         KC_RSPC,
-        TG(ACCENTS), LCTL(KC_TAB),   KC_NO,  KC_NO,  KC_NO,
+                                                             // right hand
+                                                             KC_7,        KC_8,           KC_9,   KC_0,   KC_MINS,  KC_EQUAL, KC_BSPC,
+                                                             KC_RBRC,     KC_Y,           KC_U,   KC_I,   KC_O,     KC_P,            KC_BSLS,
+                                                             KC_H,        KC_J,           KC_K,   KC_L,   KC_SCLN,  CTL_T(KC_QUOT),
+                                                             KC_NO,       KC_N,           KC_M,   KC_COMM,KC_DOT,   KC_SLSH,         KC_RSPC,
+                                                             TG(ACCENTS), LCTL(KC_TAB),   KC_NO,  KC_NO,  KC_NO,
 
-        KC_NO,       KC_NO,
-        KC_NO,
-        KC_NO,       KC_ENT,         KC_SPC
+                                                             KC_NO,       KC_NO,
+                                                             KC_NO,
+                                                             KC_NO,       KC_ENT,         KC_SPC
     ),
 
 /* Keymap 1: Gaming layer
