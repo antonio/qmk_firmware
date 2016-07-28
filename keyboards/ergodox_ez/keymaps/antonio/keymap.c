bf3eea634fea1b5279550cdea187084b48ad0b8e
Antonio Santos <antonio@santosvelasco.com>
2016-07-28 11:26:56 +0200

    Move cmd tab shortcuts to the left hand

--- keyboards/ergodox_ez/keymaps/antonio/keymap.c
+++ keyboards/ergodox_ez/keymaps/antonio/keymap.c
@@ -30,7 +30,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+------|  [{  |           |  ]}  |------+------+------+------+------+--------|
  * |LShift/(|   Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |  ,<  |  .>  |  /?  |RShift/)|
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *   |      |      |      |C-S-T | LAlt |                                       |ACCNTS| C-T  |      |      |      |
+ *   |      |      |      |      | LAlt |                                       |ACCNTS|      |      |      |      |
  *   `----------------------------------'                                       `----------------------------------'
  *                                        ,-------------.       ,-------------.
  *                                        |      |GAMES |       |      |        |
@@ -48,7 +48,7 @@ KC_GRAVE,       KC_1,   KC_2,   KC_3,             KC_4,     KC_5,   KC_6,
 KC_TAB,         KC_Q,   KC_W,   KC_E,             KC_R,     KC_T,   KC_LBRC,
 CTL_T(KC_ESC),  KC_A,   KC_S,   KC_D,             KC_F,     KC_G,
 KC_LSPO,        KC_Z,   KC_X,   KC_C,             KC_V,     KC_B,   KC_NO,
-KC_NO,          KC_NO,  KC_NO,  LCTL(S(KC_TAB)),  KC_LALT,
+KC_NO,          KC_NO,  KC_NO,  KC_NO,  KC_LALT,
 
                                                     KC_NO,  TG(GAMES),
                                                                 KC_NO,
@@ -59,7 +59,7 @@ KC_NO,          KC_NO,  KC_NO,  LCTL(S(KC_TAB)),  KC_LALT,
                                                              KC_RBRC,     KC_Y,           KC_U,   KC_I,   KC_O,     KC_P,            KC_BSLS,
                                                              KC_H,        KC_J,           KC_K,   KC_L,   KC_SCLN,  CTL_T(KC_QUOT),
                                                              KC_NO,       KC_N,           KC_M,   KC_COMM,KC_DOT,   KC_SLSH,         KC_RSPC,
-                                                             MO(ACCENTS), LCTL(KC_TAB),   KC_NO,  KC_NO,  KC_NO,
+                                                             MO(ACCENTS), KC_NO,   KC_NO,  KC_NO,  KC_NO,
 
                                                              KC_NO,       KC_NO,
                                                              KC_NO,
@@ -209,7 +209,7 @@ KC_NO,          KC_NO,  KC_NO,  LCTL(S(KC_TAB)),  KC_LALT,
  * |-----------+------+------+------+------+-------------|           |------+------+------+------+------+------+-----------|
  * |           |      |      |      |      |      |      |           |      |      |      |      |      |      |           |
  * |-----------+------+------+------+------+------|      |           |      |------+------+------+------+------+-----------|
- * |           |      |      |      |      |      |------|           |------|      |LEFT  | DOWN |  UP  | RIGHT|           |
+ * |           |      |C-S-T | C-T  |      |      |------|           |------|      |LEFT  | DOWN |  UP  | RIGHT|           |
  * |-----------+------+------+------+------+------|      |           |      |------+------+------+------+------+-----------|
  * |           |      |      |      |      |      |      |           |      |      |      |      |      |      |           |
  * `-----------+------+------+------+------+-------------'           `-------------+------+------+------+------+-----------'
@@ -228,7 +228,7 @@ KC_NO,          KC_NO,  KC_NO,  LCTL(S(KC_TAB)),  KC_LALT,
 // left hand
  RESET   ,KC_F1      ,KC_F2     ,KC_F3      ,KC_F4      ,KC_F5      ,KC_F6
 ,KC_TRNS ,KC_TRNS    ,KC_TRNS   ,KC_TRNS    ,KC_TRNS    ,KC_TRNS    ,KC_TRNS
-,KC_TRNS ,KC_TRNS    ,KC_TRNS   ,KC_TRNS    ,KC_TRNS    ,KC_TRNS
+,KC_TRNS ,KC_TRNS    ,LCTL(S(KC_TAB))        ,LCTL(KC_TAB)    ,KC_TRNS    ,KC_TRNS
 ,KC_TRNS ,KC_TRNS    ,KC_TRNS   ,KC_TRNS    ,KC_TRNS    ,KC_TRNS    ,KC_TRNS
 ,KC_TRNS ,KC_TRNS    ,KC_TRNS   ,KC_TRNS    ,KC_TRNS
 
