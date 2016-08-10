74dfd06c07ce5c98335d8e13551bb34f903f19ca
Antonio Santos <antonio@santosvelasco.com>
2016-08-10 11:53:34 +0200

    Don't use the space cadet

--- keyboards/ergodox_ez/keymaps/antonio/keymap.c
+++ keyboards/ergodox_ez/keymaps/antonio/keymap.c
@@ -24,10 +24,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,--------------------------------------------------.           ,--------------------------------------------------.
  * |   `~   |   1  |   2  |   3  |   4  |   5  |  6   |           |   7  |   8  |   9  |   0  |   -  |   =  | Bckspc |
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
- * | Tab    |   Q  |   W  |   E  |   R  |   T  |      |           |      |   Y  |   U  |   I  |   O  |   P  |   \|   |
+ * | Tab    |   Q  |   W  |   E  |   R  |   T  |  [{  |           |  ]}  |   Y  |   U  |   I  |   O  |   P  |   \|   |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
  * |Ctrl/Esc|   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |  ;:  |Ctrl/'" |
- * |--------+------+------+------+------+------|  [{  |           |  ]}  |------+------+------+------+------+--------|
+ * |--------+------+------+------+------+------|   (  |           |   )  |------+------+------+------+------+--------|
  * |LShift/(|   Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |  ,<  |  .>  |  /?  |RShift/)|
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
  *   |      |      |      |      | LAlt |                                       |ACCNTS|      |      |      |      |
@@ -47,7 +47,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 KC_GRAVE,       KC_1,   KC_2,   KC_3,             KC_4,     KC_5,   KC_6,
 KC_TAB,         KC_Q,   KC_W,   KC_E,             KC_R,     KC_T,   KC_LBRC,
 CTL_T(KC_ESC),  KC_A,   KC_S,   KC_D,             KC_F,     KC_G,
-KC_LSPO,        KC_Z,   KC_X,   KC_C,             KC_V,     KC_B,   KC_NO,
+KC_LSFT,        KC_Z,   KC_X,   KC_C,             KC_V,     KC_B,   KC_LPRN,
 KC_NO,          KC_NO,  KC_NO,  KC_NO,  KC_LALT,
 
                                                     KC_NO,  TG(GAMES),
@@ -58,7 +58,7 @@ KC_NO,          KC_NO,  KC_NO,  KC_NO,  KC_LALT,
                                                              KC_7,        KC_8,           KC_9,   KC_0,   KC_MINS,  KC_EQUAL, KC_BSPC,
                                                              KC_RBRC,     KC_Y,           KC_U,   KC_I,   KC_O,     KC_P,            KC_BSLS,
                                                              KC_H,        KC_J,           KC_K,   KC_L,   KC_SCLN,  CTL_T(KC_QUOT),
-                                                             KC_NO,       KC_N,           KC_M,   KC_COMM,KC_DOT,   KC_SLSH,         KC_RSPC,
+                                                             KC_RPRN,       KC_N,           KC_M,   KC_COMM,KC_DOT,   KC_SLSH,       KC_RSFT,
                                                              MO(ACCENTS), KC_NO,   KC_NO,  KC_NO,  KC_NO,
 
                                                              KC_NO,       KC_NO,
